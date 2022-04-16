94946e1a9538057ca2e8445c20975b8104c10448
sjkang930 <91238446+sjkang930@users.noreply.github.com>
2022-04-16 01:12:11 -0700

    first commitg

--- databaseAccessLayer.js
+++ databaseAccessLayer.js
@@ -14,42 +14,6 @@ function getAllUsers(callback) {
 	});
 }
 
-// const passwordPepper = "SeCretPeppa4MySal+";
-// function addUser(postData, callback) {
-// 	let sqlInsertSalt = "INSERT INTO web_user (first_name, last_name, email, password_salt)VALUES (:first_name, :last_name, :email, sha2(UUID(),512));"
-// 	let params = {
-// 		first_name: postData.first_name,
-// 		last_name: postData.last_name,
-// 		email: postData.email
-// 	};
-// 	console.log(sqlInsertSalt);
-// 	database.query(sqlInsertSalt, params, (err, results, fields) => {
-// 		if (err) {
-// 			console.log(err);
-// 			callback(err, null);
-// 		}
-// 		else {
-// 			let insertedID = results.insertId;
-// 			let updatePasswordHash = "UPDATE web_user SET password_hash = sha2(concat(:password,:pepper,password_salt),512) WHERE web_user_id = :userId;"
-// 			let params2 = {
-// 				password: postData.password,
-// 				pepper: passwordPepper,
-// 				userId: insertedID
-// 			}
-// 			console.log(updatePasswordHash);
-// 			database.query(updatePasswordHash, params2, (err, results, fields) => {
-// 				if (err) {
-// 					console.log(err);
-// 					callback(err, null);
-// 				}
-// 				else {
-// 					console.log(results);
-// 					callback(null, results);
-// 				}
-// 			});
-// 		}
-// 	});
-// }
 const passwordPepper = "SeCretPeppa4MySal+";
 function addUser(postData, callback) {
 	let sqlInsertSalt = "INSERT INTO web_user (first_name, last_name, email, password_salt)VALUES (:first_name, :last_name, :email, sha2(UUID(), 512));";
